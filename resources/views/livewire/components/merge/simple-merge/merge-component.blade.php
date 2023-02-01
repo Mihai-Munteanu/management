2a2bb5b3e4919ee97abca9d5f6453b3e9dd22c8f
MihaiM <Mihai_Munteanu_90@yahoo.com>
2023-02-01 14:50:13 +0200

    add button to table view

--- resources/views/livewire/components/merge/simple-merge/merge-component.blade.php
+++ resources/views/livewire/components/merge/simple-merge/merge-component.blade.php
@@ -1,27 +1,35 @@
-<div class="w-full flex justify-center pt-12">
-    <div class="container">
-        <div class="flex flex-col lg:flex-row justify-between lg:space-x-10">
-            <div class="w-full">
-                <livewire:components.merge.simple-merge.single-item-select-api wire:key=time() tableName="{{$tableName}} Merge Into" tableApi="{{$tableApi}}" />
+<div>
+    <div class="w-full flex justify-end container px-12 py-4">
+        <x-anchor-button href="{{route('mapper.' . $tableApi)}}">
+            Table View
+        </x-anchor-button>
+    </div>
+
+    <div class="w-full flex justify-center">
+        <div class="container">
+            <div class="flex flex-col lg:flex-row justify-between lg:space-x-10">
+                <div class="w-full">
+                    <livewire:components.merge.simple-merge.single-item-select-api wire:key=time() tableName="{{$tableName}} Merge Into" tableApi="{{$tableApi}}" />
+                </div>
+
+                <div class="flex items-center">
+                    <x-button wire:click="syncMapping">
+                        Sync
+                    </x-button>
+                </div>
+
+                <div class="w-full">
+                    <livewire:components.merge.simple-merge.multiple-items-select-api tableName="{{$tableName}} Merge From" tableApi="{{$tableApi}}" />
+                </div>
             </div>
 
-            <div class="flex items-center">
-                <x-button wire:click="syncMapping">
-                    Sync
-                </x-button>
+            <div>
+                <livewire:components.merge.simple-merge.show-current-selected-items />
             </div>
 
-            <div class="w-full">
-                <livewire:components.merge.simple-merge.multiple-items-select-api tableName="{{$tableName}} Merge From" tableApi="{{$tableApi}}" />
+            <div>
+                <livewire:components.merge.simple-merge.mapping-table tableName="{{$tableName}} Merge Into" tableApi="{{$tableApi}}" />
             </div>
-        </div>
-
-        <div>
-            <livewire:components.merge.simple-merge.show-current-selected-items />
-        </div>
-
-        <div>
-            <livewire:components.merge.simple-merge.mapping-table tableName="{{$tableName}} Merge Into" tableApi="{{$tableApi}}" />
         </div>
     </div>
 </div>
